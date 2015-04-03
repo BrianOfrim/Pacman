cad31d289165aa20741acc707a2b07677ca235ed
Asanche <asanche@ualberta.ca>
2015-04-03 17:53:15 -0600

    Drew a map

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ if __name__ == "__main__" :
     argv = sys.argv[1:]
     main_gui = GUI(screen_height, screen_width)
     #pygame.draw.circle(screen,BLUE,(100,100),50,0 )
+    main_gui.map()
     
     while 1:
         for event in pygame.event.get():
