736032075760fa17ddfcc9c05e7bf52b35911bbf
BrianOfrim <bofrim@ualberta.ca>
2015-04-03 19:45:22 -0600

    gui update graph added

--- gui.py
+++ gui.py
@@ -1,14 +1,12 @@
 import sys, pygame
-from graph_V2 import Graph
+from graph_v2 import Graph
 
 class GUI():
     BG_COLOR = (32, 32, 32)
     
     
     def __init__(self,screen_height,screen_width,
-
-
-                 score_rect=(0,550,600,50),score_color =(0,0,255) ):
+                 score_rect=(0,550,600,50),score_color =(0,0,255)):
 
 
         #Initialize screen
@@ -23,9 +21,15 @@ class GUI():
 
 
     def draw_rect(self,screen,color,rect):
-        pygame.draw.rect(screen,color,rect)
+        return pygame.draw.rect(screen,color,rect)
+
+    def draw_circle(self,screen, color, pos, radius):
+        return pygame.draw.circle(screen, color, pos, radius)
 
     def map(self):
+        edges = list()
+        verticies = set()
+        pac_dot_status = {}
         tile_dim = 25
         maparray = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                     [0,0,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,0,0],
@@ -53,4 +57,20 @@ class GUI():
             for x in range(0,len(maparray[y])):
                 if maparray[y][x] == 0:
                     self.draw_rect(self.screen,(0,0,255),(25*x,25*y,25,25))
->>>>>>> origin/master
+                else:
+                    self.draw_circle(self.screen,(222,253,68),
+                                     (25*x+12,25*y+12), 2)
+                    verticies.add((x*tile_dim, y*tile_dim))
+                    pac_dot_status[(x*tile_dim, y*tile_dim)] = 1
+                    #check for left neighbour
+                    if(x!=0 and maparray[y][x-1] == 1):
+                        edges.append(((x*tile_dim, y*tile_dim),
+                                     ((x-1)*tile_dim, y*tile_dim)))
+                    #check for upper neighbour
+                    if(y!=0 and maparray[y-1][x] == 1):
+                        edges.append(((x*tile_dim, y*tile_dim),
+                                     (x*tile_dim, (y-1)*tile_dim)))
+        return Graph(verticies,edges), pac_dot_status
+                           
+                
+                    
