3ff78aed27a370cda7c5e703dd2380f1b38c4c67
BrianOfrim <bofrim@ualberta.ca>
2015-04-07 15:03:54 -0600

    clyde started

---
+++ ghost2.py
@@ -0,0 +1,7 @@
+import pygame, gui, process_path, math,pdb
+from pygame.sprite import Sprite
+from binary_heap import BinaryHeap
+from ghost import ghost
+
+#class clyde(ghost):
+    #def __init__(self,x,y,path_
