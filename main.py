2670c4c980963274b09a4e5097eed5c0e5655ee1
Asanche <asanche@ualberta.ca>
2015-04-07 16:22:52 -0600

    Eating Stuff

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ if __name__ == "__main__" :
     pacguy = main_gui.print_pacman()
     temp_map = main_gui.map()
     pacguy.map = temp_map
-    ghost1 = ghost(9*25,3*25,temp_map )
+    ghost1 = ghost(9*25,3*25,temp_map)
     main_gui.ghost_list.add(ghost1)
-
 
     edge_list = pacguy.map.edges()
     #for edge in edge_list:
@@ -43,10 +42,7 @@ if __name__ == "__main__" :
                     (event.key == pygame.K_LEFT) or
                     (event.key == pygame.K_UP) or (event.key == pygame.K_DOWN)):
                     pacguy.MoveKeyDown(event.key)
-
-        main_gui.ghost_list.add(ghost1)
-
-         
+     
 
         pellet_hit_list = pygame.sprite.spritecollide(pacguy,
                                                       main_gui.pellet_list, 
@@ -54,26 +50,47 @@ if __name__ == "__main__" :
         ppellet_hit_list = pygame.sprite.spritecollide(pacguy,
                                                       main_gui.ppellet_list,
                                                       False)
-
         ghost_hit_list = pygame.sprite.spritecollide(pacguy,
                                                       main_gui.ghost_list,
                                                       False)
-              
         for ghost in ghost_hit_list:
             #pdb.set_trace()
-            pacguy.die()
-            pacguy.kill()
-            ghost.kill()
-            pacguy.start() 
+            if pacguy.power == 0:
+                pacguy.die()
+                ghost.kill()
+                pacguy.kill()
+                pacguy.start() 
+            elif pacguy.power == 1:
+                ghost.kill()
+                pacguy.score += 500
+                #ghost1 = ghost(9*25,3*25,temp_map)      
            
-                
         main_gui.ghost_list.draw(main_gui.screen)
         for pellet in pellet_hit_list:
             pacguy.score += 100
             pellet.kill()
+
         for ppellet in ppellet_hit_list:
+            if pacguy.power == 1:
+                clock1 = int(clock.get_rawtime())
             pacguy.power = 1
             ppellet.kill()
+            ghost1.image = ghost1.image2
+            ghost1.imgnum = 2
+            ghost1.newimgrot()
+        if pacguy.power == 1:
+            if pacguy.power == 1 and pacguy.derp == 0:
+                clock1 = int(clock.get_rawtime())
+                pacguy.derp +=1
+            print(clock1)
+            if int(clock.get_rawtime()) >= (clock1 + 60):
+                pacguy.power = 0
+                pacguy.derp = 0
+                ghost1.image = ghost1.image1
+                ghost1.imgnum = 1
+                ghost1.newimgrot()
+
+
         main_gui.draw_background()
         main_gui.draw_map()
         main_gui.pacman_and_pellets.draw(main_gui.screen)
