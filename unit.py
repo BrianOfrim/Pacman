1004e4716f44836e0003b2532cdb11bcd466d2c9
Asanche <asanche@ualberta.ca>
2015-04-07 12:33:25 -0600

    April 7th

--- unit.py
+++ unit.py
@@ -9,6 +9,7 @@ class pacman(Sprite):
     printed to the map.
     """
     sprite = pygame.image.load("Assets/pacman.png")
+    sprite1 = pygame.image.load("Assets/circle.png")
     def __init__(self,
                  loc_x = None,
                  loc_y = None,
@@ -53,9 +54,15 @@ class pacman(Sprite):
         #self.die_sound
         
         self.image = pacman.sprite.convert()
+        self.image1 = pacman.sprite.convert()
+        self.image2 = pacman.sprite1.convert()
+        self.imgnum = 1
+        self.loopcount = 0
         self.base_image = self.image
         self.rect = self.image.get_rect()
         self.image.set_colorkey((32,32,32))
+        self.image1.set_colorkey((32,32,32))
+        self.image2.set_colorkey((32,32,32))
         #def move(self):
         self.current_node = None
         self.next_node = None
@@ -73,20 +80,29 @@ class pacman(Sprite):
         self.rect.x = 11*25
         
    
-
-    def newangle(self,key):
-        if (key == pygame.K_RIGHT):
+    def newimgrot(self):
+        if self.angle == 0:
             self.image = pygame.transform.rotate(self.base_image,0)
+        if self.angle == 90:
+            self.image = pygame.transform.rotate(self.base_image,90)
+        if self.angle == 180:
+            self.image = pygame.transform.rotate(self.base_image,180)
+        if self.angle == 270:
+            self.image = pygame.transform.rotate(self.base_image,270)
+
+    """def newangle(self,key):
+        if (key == pygame.K_RIGHT):
+            self.image1 = pygame.transform.rotate(self.base_image,0)
             self.angle = 0
         if (key == pygame.K_LEFT):
-            self.image = pygame.transform.rotate(self.base_image,180)
+            self.image1 = pygame.transform.rotate(self.base_image,180)
             self.angle = 180
         if (key == pygame.K_UP):
-            self.image = pygame.transform.rotate(self.base_image,90)
+            self.image1 = pygame.transform.rotate(self.base_image,90)
             self.angle = 90
         if (key == pygame.K_DOWN):
-            self.image = pygame.transform.rotate(self.base_image,270)
-            self.angle = 270
+            self.image1 = pygame.transform.rotate(self.base_image,270)
+            self.angle = 270"""
 
     def process_angle(self, key):
         if (key == pygame.K_RIGHT):
@@ -97,6 +113,7 @@ class pacman(Sprite):
             requested_angle = 180
         if (key == pygame.K_DOWN):
             requested_angle = 270
+        
         #check if directions are opposite
         if(requested_angle == ((self.angle + 180) % 360) 
            or self.move_state =='D'):
@@ -161,6 +178,15 @@ class pacman(Sprite):
             self.rect.x -= 5
         if self.angle == 270:
             self.rect.y += 5
+        self.loopcount +=1
+        """if self.imgnum == 1 and self.loopcount == 3:
+            self.image = self.image2
+            self.imgnum = 2
+            self.loopcount = 0
+        if self.imgnum == 2 and self.loopcount == 3:
+            self.image = self.image1
+            self.imgnum = 1
+            self.loopcount == 0"""
         #print("")
         #print("Pacman x {}".format(self.rect.x))
         #print("Pacman y {}".format(self.rect.y))
